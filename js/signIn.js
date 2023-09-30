b139c12f3c8c36aa85a6034b39b83aa3e6ff4251
Korny94 <karlmagnusnokling@hotmail.no>
2023-09-30 04:52:34 +0200

    Login authentication and error message fix

--- js/signIn.js
+++ js/signIn.js
@@ -1,17 +1,43 @@
 const loginEmail = document.querySelector("#loginEmail");
 const loginPassword = document.querySelector("#loginPassword");
 const submitLogin = document.querySelector("#submitLogin");
+const popoverMessage = document.querySelector("#popoverMessage");
+const popoverMessage2 = document.querySelector("#popoverMessage2");
+const loginFailed = document.querySelector("#loginFailed");
 
 submitLogin.addEventListener("click", (event) => {
   event.preventDefault();
-  localStorage.setItem("email", loginEmail.value);
-  localStorage.setItem("password", loginPassword.value);
+
+  // Get the email and password values from the form
+  const email = loginEmail.value;
+  const password = loginPassword.value;
+
+  // Define regular expressions for email format and password length
+  const emailRegex = /^[a-zA-Z0-9._-]+@(noroff\.no|stud\.noroff\.no)$/i;
+  const passwordRegex = /^.{8,}$/;
+
+  // Check if the email and password match the required formats
+  if (!emailRegex.test(email)) {
+    popoverMessage.innerText = "Please use @noroff.no or @stud.noroff.no.";
+    loginEmail.classList.add("border-danger");
+    return;
+  } else loginEmail.classList.remove("border-danger");
+  loginEmail.classList.add("border-success");
+  popoverMessage.innerText = "";
+
+  if (!passwordRegex.test(password)) {
+    popoverMessage2.innerText = "Password must be at least 8 characters long.";
+
+    loginPassword.classList.add("border-danger");
+    return;
+  } else loginPassword.classList.remove("border-danger");
+
+  // For example, you can send a login request to your API
   const userToLogin = {
-    email: loginEmail.value,
-    password: loginPassword.value,
+    email: email,
+    password: password,
   };
   const loginUrl = `${API_BASE_URL}/api/v1/social/auth/login`;
-
   loginUser(loginUrl, userToLogin);
 });
 
@@ -45,6 +71,9 @@ async function loginUser(url, userData) {
     localStorage.setItem("status", response.ok);
     if (response.ok) {
       window.location.href = "/html/profile.html";
+    } else {
+      loginFailed.classList.add("text-danger");
+      loginFailed.innerText = "Login failed, please try again.";
     }
   } catch (error) {
     console.log(error);
